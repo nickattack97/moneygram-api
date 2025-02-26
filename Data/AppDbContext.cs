58285bfbacdaa409a426551176f8eb898f096e70
tdhliwayo <tdhliwayo@cbz.co.zw>
2025-02-26 19:32:48 +0200

    Refactor AppDbContext: streamline constructor and add new DbSet properties for CodeTable, CountryInfoEntity, and CurrencyInfo

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -5,15 +5,15 @@ namespace moneygram_api.Data
 {
     public class AppDbContext : DbContext
     {
-        public AppDbContext(DbContextOptions<AppDbContext> options)
-            : base(options)
-        {
-        }
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
         public DbSet<RequestLog> RequestLogs { get; set; }
         public DbSet<ExceptionLog> ExceptionLogs { get; set; }
-        public DbSet<SendTransaction> SendTransactions { get; set; } 
-    
+        public DbSet<SendTransaction> SendTransactions { get; set; }
+        public DbSet<CodeTable> CodeTables { get; set; }
+        public DbSet<CountryInfoEntity> CountriesInfo { get; set; }
+        public DbSet<CurrencyInfoEntity> CurrencyInfo { get; set; }
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
@@ -22,11 +22,14 @@ namespace moneygram_api.Data
             {
                 entity.Property(e => e.SendAmount).HasColumnType("decimal(18,2)");
                 entity.Property(e => e.ReceiveAmount).HasColumnType("decimal(18,2)");
-                entity.Property(e => e.SendAmount).HasColumnType("decimal(18,2)");
                 entity.Property(e => e.ExchangeRate).HasColumnType("decimal(18,4)");
                 entity.Property(e => e.Charge).HasColumnType("decimal(18,2)");
                 entity.Property(e => e.TotalAmountCollected).HasColumnType("decimal(18,2)");
             });
+
+            modelBuilder.Entity<CodeTable>().ToTable("CodeTables");
+            modelBuilder.Entity<CountryInfoEntity>().ToTable("CountriesInfo");
+            modelBuilder.Entity<CurrencyInfoEntity>().ToTable("CurrencyInfo");
         }
     }
 }
