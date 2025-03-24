a66ed12f86b9ec4459d7fbc696fb2a4dad19b05a
tdhliwayo <tdhliwayo@cbz.co.zw>
2025-03-24 23:46:54 +0200

    feat: enhance LoggingService with webhook logging capabilities and update ILoggingService interface

--- Services/Implementations/LoggingService.cs
+++ Services/Implementations/LoggingService.cs
@@ -1,103 +1,110 @@
+using moneygram_api.Data;
 using moneygram_api.Models;
 using moneygram_api.Services.Interfaces;
-using moneygram_api.Data;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
 using System;
-using System.Threading.Tasks;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace moneygram_api.Services.Implementations
 {
     public class LoggingService : ILoggingService
     {
-        private readonly IServiceProvider _serviceProvider;
+        private readonly AppDbContext _context;
 
-        public LoggingService(IServiceProvider serviceProvider)
+        public LoggingService(AppDbContext context)
         {
-            _serviceProvider = serviceProvider;
+            _context = context;
         }
 
         public async Task LogRequestAsync(RequestLog requestLog)
         {
-            using (var scope = _serviceProvider.CreateScope())
-            {
-                var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-                context.RequestLogs.Add(requestLog);
-                await context.SaveChangesAsync();
-            }
-        }
-
-        public async Task LogExceptionAsync(ExceptionLog exceptionLog)
-        {
-            using (var scope = _serviceProvider.CreateScope())
-            {
-                var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-                context.ExceptionLogs.Add(exceptionLog);
-                await context.SaveChangesAsync();
-            }
+            _context.RequestLogs.Add(requestLog);
+            await _context.SaveChangesAsync();
         }
 
         public async Task<RequestLog> GetRequestLogByIdAsync(int id)
         {
-            using (var scope = _serviceProvider.CreateScope())
-            {
-                var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-                return await context.RequestLogs.FindAsync(id);
-            }
+            return await _context.RequestLogs.FindAsync(id);
         }
 
         public async Task UpdateRequestLogAsync(RequestLog requestLog)
         {
-            using (var scope = _serviceProvider.CreateScope())
-            {
-                var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-                context.RequestLogs.Update(requestLog);
-                await context.SaveChangesAsync();
-            }
+            _context.RequestLogs.Update(requestLog);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task LogExceptionAsync(ExceptionLog exceptionLog)
+        {
+            _context.ExceptionLogs.Add(exceptionLog);
+            await _context.SaveChangesAsync();
         }
 
         public async Task LogMoneyGramXmlAsync(MoneyGramXmlLog xmlLog)
         {
-            using (var scope = _serviceProvider.CreateScope())
-            {
-                var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-                context.MoneyGramXmlLogs.Add(xmlLog);
-                await context.SaveChangesAsync();
-            }
+            _context.MoneyGramXmlLogs.Add(xmlLog);
+            await _context.SaveChangesAsync();
         }
 
         public async Task<MoneyGramXmlLog> GetMoneyGramXmlLogByIdAsync(int id)
         {
-            using (var scope = _serviceProvider.CreateScope())
-            {
-                var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-                return await context.MoneyGramXmlLogs.FindAsync(id);
-            }
+            return await _context.MoneyGramXmlLogs.FindAsync(id);
         }
 
         public async Task<List<MoneyGramXmlLog>> GetMoneyGramXmlLogsAsync(
-            DateTime? startDate = null, 
-            DateTime? endDate = null, 
+            DateTime? startDate = null,
+            DateTime? endDate = null,
             string operation = null)
         {
-            using (var scope = _serviceProvider.CreateScope())
-            {
-                var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-                var query = context.MoneyGramXmlLogs.AsQueryable();
+            var query = _context.MoneyGramXmlLogs.AsQueryable();
 
-                if (startDate.HasValue)
-                    query = query.Where(l => l.LogTime >= startDate.Value);
-                
-                if (endDate.HasValue)
-                    query = query.Where(l => l.LogTime <= endDate.Value);
-                
-                if (!string.IsNullOrEmpty(operation))
-                    query = query.Where(l => l.Operation == operation);
+            if (startDate.HasValue)
+                query = query.Where(l => l.LogTime >= startDate.Value);
 
-                return await query.OrderByDescending(l => l.LogTime).ToListAsync();
-            }
+            if (endDate.HasValue)
+                query = query.Where(l => l.LogTime <= endDate.Value);
+
+            if (!string.IsNullOrEmpty(operation))
+                query = query.Where(l => l.Operation == operation);
+
+            return await query.OrderByDescending(l => l.LogTime).ToListAsync();
+        }
+
+        public async Task LogWebhookRequestAsync(WebhookLog webhookLog)
+        {
+            _context.WebhookLogs.Add(webhookLog);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<WebhookLog> GetWebhookLogByIdAsync(int id)
+        {
+            return await _context.WebhookLogs.FindAsync(id);
+        }
+
+        public async Task UpdateWebhookLogAsync(WebhookLog webhookLog)
+        {
+            _context.WebhookLogs.Update(webhookLog);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<List<WebhookLog>> GetWebhookLogsAsync(
+            DateTime? startDate = null,
+            DateTime? endDate = null,
+            string transactionStatus = null)
+        {
+            var query = _context.WebhookLogs.AsQueryable();
+
+            if (startDate.HasValue)
+                query = query.Where(l => l.RequestTime >= startDate.Value);
+
+            if (endDate.HasValue)
+                query = query.Where(l => l.RequestTime <= endDate.Value);
+
+            if (!string.IsNullOrEmpty(transactionStatus))
+                query = query.Where(l => l.RequestBody.Contains($"\"transactionStatus\":\"{transactionStatus}\"", StringComparison.OrdinalIgnoreCase));
+
+            return await query.OrderByDescending(l => l.RequestTime).ToListAsync();
         }
     }
 }
--- Services/Interfaces/ILoggingService.cs
+++ Services/Interfaces/ILoggingService.cs
@@ -1,4 +1,6 @@
 using moneygram_api.Models;
+using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace moneygram_api.Services.Interfaces
@@ -12,8 +14,15 @@ namespace moneygram_api.Services.Interfaces
         Task LogMoneyGramXmlAsync(MoneyGramXmlLog xmlLog);
         Task<MoneyGramXmlLog> GetMoneyGramXmlLogByIdAsync(int id);
         Task<List<MoneyGramXmlLog>> GetMoneyGramXmlLogsAsync(
-            DateTime? startDate = null, 
-            DateTime? endDate = null, 
+            DateTime? startDate = null,
+            DateTime? endDate = null,
             string operation = null);
+        Task LogWebhookRequestAsync(WebhookLog webhookLog);
+        Task<WebhookLog> GetWebhookLogByIdAsync(int id);
+        Task UpdateWebhookLogAsync(WebhookLog webhookLog);
+        Task<List<WebhookLog>> GetWebhookLogsAsync(
+            DateTime? startDate = null,
+            DateTime? endDate = null,
+            string transactionStatus = null);
     }
 }
