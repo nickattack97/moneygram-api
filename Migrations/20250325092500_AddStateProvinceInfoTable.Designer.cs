b79aef5cf5af99e8bef739ef6773b048127e7cea
tdhliwayo <tdhliwayo@cbz.co.zw>
2025-03-25 13:19:36 +0200

    feat: add StateProvinceInfo table to track state and province details

--- Migrations/20250325092500_AddStateProvinceInfoTable.Designer.cs
+++ Migrations/20250325092500_AddStateProvinceInfoTable.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using moneygram_api.Data;
 
@@ -11,9 +12,11 @@ using moneygram_api.Data;
 namespace moneygram_api.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250325092500_AddStateProvinceInfoTable")]
+    partial class AddStateProvinceInfoTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
