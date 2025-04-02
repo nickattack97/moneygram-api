bc40d94776d4a9b35d8c70fa59e3d5e604386356
tdhliwayo <tdhliwayo@cbz.co.zw>
2025-04-02 23:08:43 +0200

    feat: update MoneyGramXmlLog model to use XML data type for RequestXml and ResponseXml properties

--- Models/MoneyGramXmlLog.cs
+++ Models/MoneyGramXmlLog.cs
@@ -1,4 +1,4 @@
-using System;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace moneygram_api.Models
 {
@@ -6,12 +6,16 @@ namespace moneygram_api.Models
     {
         public long Id { get; set; }
         public string Operation { get; set; }
+        
+        [Column(TypeName = "xml")]
         public string RequestXml { get; set; }
+        
+        [Column(TypeName = "xml")]
         public string ResponseXml { get; set; }
+        
         public string HttpMethod { get; set; }
         public string Url { get; set; }
         public string Username { get; set; }
         public DateTime LogTime { get; set; }
-
     }
 }
